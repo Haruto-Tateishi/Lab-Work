09ea0eee09884d0b3810b8b0632bf66c4360f089
Haruto-Tateishi <haruto111908tate@gmail.com>
2024-06-01 19:46:43 -0400

    second commit

---
+++ 1kG.BEDstartpoint.py
@@ -0,0 +1,23 @@
+# this script checks if the starting points of the intervals of BED files are sorted or not
+
+fn1 = "Chr22.regulation.RefSeq.txt"
+f1 = open(fn1, "r")
+
+fn2 = "sample.txt"
+f2 = open(fn2, "w")
+
+intervals = f1.readlines()
+num_line = len(intervals)
+
+for i in range(num_line - 1):
+  line1 = intervals[i].split("\t")
+  line2 = intervals[i+1].split("\t")
+  if line1[0] == "chr22":
+    start1 = int(line1[1])
+    start2 = int(line2[1])
+    # if start1 < start2:
+      # print("yes")
+    if start1 > start2:
+      # print("no")
+      # print(str(i) + "\t" + str(start1) + "\t" + str(start2))
+      f2.write(str(i+1) + "\t" + str(start1) + "\t" + str(start2) + "\n")
