9aa11234c1416c3f08681ea8f49378d6284797b2
Haruto-Tateishi <haruto111908tate@gmail.com>
2024-07-22 12:44:47 -0400

    commit 07/22/24

--- SFS-integration.py
+++ SFS-integration.py
@@ -2,6 +2,7 @@ from PRF_Ratios import PRF_Ratios_functions as prf
 import numpy as np
 import scipy.integrate as integrate
 import scipy.special as special
+import pickle 
 
 def sim_integration(theta, g, nc, misspec, maxi, returnexpected, output_file):
     a_list = []
@@ -38,21 +39,54 @@ def sim_integration(theta, g, nc, misspec, maxi, returnexpected, output_file):
     output_file.write(line)
 
 def data_integration(conseq, data_line, output_file):
-    data_list = data_line.strip().split(sep="\t")
-    np_array = np.array(data_list)
+    data_list = data_line.strip().split()
+    float_list = [float(x) for x in data_list]
+    np_array = np.array(float_list)
     sum = np.cumsum(np_array)
-    relative = sum / sum[-1]
-    result = integrate.cumulative_trapezoid(relative, x=None, dx=1.0, axis=-1, initial=None)
-    integrate = result[-1]
+    try:
+        relative = sum / sum[-1]
+        result = integrate.cumulative_trapezoid(relative, x=None, dx=1.0, axis=-1, initial=None)
+        auc = result[-1]
 
-    line = [conseq, str(integrate), "\n"]
-    line = "\t".join(line)
-    output_file.write(line)
+        line = [conseq, str(auc), "\n"]
+        line = "\t".join(line)
+        output_file.write(line)
+    except RuntimeWarning:
+        line = [conseq, "No Curve", "\n"]
+        line = "\t".join(line)
+        output_file.write(line)
 
-fn1 = "SFS-downsample-integrate.tsv"
-f1 = open(fn1, "a")
+def dict_integration(pickle_file, nc, output_file):
+    Dic = pickle.load(open(pickle_file, "rb"))
+    for conseq in Dic:
+        sfs = np.zeros(nc, dtype=np.float32)
+        for acan in Dic[conseq]:
+            # an = acan.split(sep="_")[3]
+            ac = int(acan.split(sep="_")[1])
+            sfs[ac-1] += Dic[conseq][acan]
+        np_array = np.array(sfs)
+        sum = np.cumsum(np_array)
+        try:
+            relative = sum / sum[-1]
+            result = integrate.cumulative_trapezoid(relative, x=None, dx=1.0, axis=-1, initial=None)
+            auc = result[-1]
 
-fn2 = "gnomad-vep-downsample-1000000.txt"
+            line = [conseq, str(auc), "\n"]
+            line = "\t".join(line)
+            output_file.write(line)
+        except RuntimeWarning:
+            line = [conseq, "No Curve", "\n"]
+            line = "\t".join(line)
+            output_file.write(line)
+    Dic.close()
+
+
+
+fn1 = "SFS-downsample-integrate-all-5008.tsv"
+f1 = open(fn1, "w")
+f1.write("conseq\tarea-under-curve\n")
+
+fn2 = "gnomad-vep-downsample-5008.txt"
 f2 = open(fn2, "r")
 
 lines = f2.readlines()
@@ -63,6 +97,9 @@ while True:
         break 
     else:
         conseq = lines[i].strip()
+        # if "&" in conseq:
+        #     i +=2
+        #     continue
         i +=1
         data_line = lines[i]
         i +=1
@@ -71,3 +108,14 @@ while True:
 
 f1.close()
 f2.close()
+
+
+# fn1 = "1kG-SFS-downsample-integrate.tsv"
+# f1 = open(fn1, "w")
+# f1.write("conseq\tarea-under-curve\n")
+
+# fn2 = "1kG_vep_consequence_ANAC_counts.p"
+
+# dict_integration(fn2, 5008, f1)
+
+# f1.close()
--- SFS-rel-cum.py
+++ SFS-rel-cum.py
@@ -3,10 +3,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import random
+import pickle
 
-def curve_color():
-    # pick up color that does not overlap
-    return None
+colors = ['black', 'blue', 'red', 'maroon', 'orange', 'greenyellow', 'green', 'cyan', 'violet', 'tan', 'gold', 'darkgrey', 'deepskyblue', 'darkorchid', 'salmon', 'tab:olive', 'tab:brown', 'mediumseagreen', 'steelblue', 'indigo', 'deeppink', 'teal', 'cornflowerblue', 'khaki', 'peru', 'palevioletred', 'lightgrey', 'rosybrown', 'darkkhaki', 'yellow', 'palegreen', 'aquamarine', 'darkgreen', 'lightcyan', 'lightskyblue', 'lightslategrey', 'mediumvioletred', 'pink', 'darkred', 'chocolate', 'firebrick', 'purple', 'cadetblue', 'mediumaquamarine', 'lime', 'lemonchiffon', 'wheat', 'whitesmoke', 'tomato', 'thistle', 'plum', 'royalblue', 'lavender', 'mediumturquoise']
+def curve_color(colors):
+    color = colors[0]
+    colors.remove(color)
+    # print(colors)
+    return color
 
 def rel_cum(conseq, data_line, curve_color):
     data = data_line.strip().split()
@@ -19,12 +24,28 @@ def rel_cum(conseq, data_line, curve_color):
     x_axis = np.arange(1, len(relative_cumulative) + 1)
     plt.plot(x_axis, relative_cumulative, marker='', linestyle='-', color=f'{curve_color}', label=f'{conseq}')
 
-fn1 = "gnomad-vep-downsample-1000000.txt"
-f1 = open(fn1, "r")
+def pkl_rel_cum(pickle_file, nc):
+    Dic = pickle.load(open(pickle_file, "rb"))
+    for conseq in Dic:
+        sfs = np.zeros(nc, dtype=np.float32)
+        c_color = curve_color(colors)
+        for acan in Dic[conseq]:
+            # an = acan.split(sep="_")[3]
+            ac = int(acan.split(sep="_")[1])
+            sfs[ac-1] += Dic[conseq][acan]
+        np_array = np.array(sfs)
+        sum = np.cumsum(np_array)
+        try:
+            relative = sum / sum[-1]
+            x_axis = np.arange(1, len(relative) + 1)
+            plt.plot(x_axis, relative, marker='', linestyle='-', color=f'{c_color}', label=f'{conseq}')
+        except RuntimeWarning:
+            pass
 
-lines = f1.readlines()
-lines_len = len(lines)
-nc = lines[0].strip()
+
+fn1 = "gnomad-vep-downsample-10000.txt"
+f1 = open(fn1, 'r')
+nc = 1000000
 
 plt.figure(figsize=(10, 6))
 plt.xlabel('Data')
@@ -32,19 +53,26 @@ plt.ylabel('Relative Cumulative Sum')
 plt.title(f'Relative Cumulative Chart with nc value = {str(nc)}')
 plt.grid(True)
 
+lines = f1.readlines()
+input_len = len(lines)
 i = 2
 while True:
-    if i == lines_len:
-        break
+    if i == input_len:
+        break 
     else:
-        conseq = lines[i]
+        conseq = lines[i].strip()
+        # if "&" in conseq:
+        #     i +=2
+        #     continue
         i +=1
         data_line = lines[i]
         i +=1
-        color = curve_color()
+        color = curve_color(colors)
         rel_cum(conseq, data_line, color)
         pass
 
 
-plt.legend(loc='lower right', fontsize='small', ncol=3, frameon=True)
+
+plt.ylim(0.985, 1.0001)
+plt.legend(loc='lower right', fontsize='small', ncol=2, frameon=True)
 plt.show()
